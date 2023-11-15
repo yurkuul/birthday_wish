fecbd94a1ae9bc528f40854452521e9e6d16f3a5
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-11-15 15:23:11 -0600

    Update fireworks.js

--- fireworks.js
+++ fireworks.js
@@ -28,3 +28,48 @@ class Particle {
         this.alpha -= 0.01;
     }
 }
+
+class Firework {
+    constructor(x, y, color, size) {
+        this.x = x;
+        this.y = y;
+        this.initialY = canvas.height;
+        this.color = color;
+        this.size = size;
+        this.velocity = { x: 0, y: Math.random() * -2.5 - 0.5 };
+        this.particles = [];
+        this.lifespan = 180;
+        this.hasExploded = false;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    explode() {
+        for (let i = 0; i < 50; i++) {
+            const particleSize = Math.random() * 3 + 1; // Random size between 1 and 4
+            this.particles.push(new Particle(this.x, this.y, this.color, particleSize));
+        }
+    }
+
+    update() {
+        this.lifespan--;
+
+        if (this.lifespan <= 0 && !this.hasExploded) {
+            this.explode();
+            this.velocity = { x: 0, y: 0 };
+            this.hasExploded = true;
+        } else if (this.lifespan > 0) {
+            this.y += this.velocity.y;
+        }
+
+        for (let i = 0; i < this.particles.length; i++) {
+            this.particles[i].update();
+            this.particles[i].draw();
+        }
+    }
+}
