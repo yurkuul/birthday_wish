b9085107dcdb21e40640f082687f73cae181c6c5
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-11-28 16:58:12 -0600

    Create balloons.js

---
+++ balloons.js
@@ -0,0 +1,23 @@
+const balloonContainer = document.createElement("div");
+balloonContainer.id = "balloon-container";
+document.querySelector('body').appendChild(balloonContainer);
+
+function random(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function getRandomStyles() {
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    var mt = random(200);
+    var ml = random(50);
+    var dur = random(5) + 5;
+    return `
+    background-color: rgba(${r},${g},${b},0.7);
+        color: rgba(${r},${g},${b},0.7); 
+        box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+        margin: ${mt}px 0 0 ${ml}px;
+        animation: float ${dur}s ease-in infinite;
+  `;
+}
